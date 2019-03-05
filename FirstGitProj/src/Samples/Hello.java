f868665d0fc3dee048e4dfd6f00bb4183a43077c
483933 <483933@LT085767.cts.com>
2019-03-05 22:51:02 +0530

    third commit

--- FirstGitProj/src/Samples/Hello.java
+++ FirstGitProj/src/Samples/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello.How are you ?");
-System.out.println("Hey");
+System.out.println("Hey,it is a test");
 	}
 
 }
