0b4640f51368a982f3472efb2ca380a0dc2d4e2e
RiyaBasak <48254049+RiyaBasak@users.noreply.github.com>
2019-03-05 23:29:26 +0530

    Update Hello.java
    changes from git

--- FirstGitProj/src/Samples/Hello.java
+++ FirstGitProj/src/Samples/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 System.out.println("Hello.How are you ?");
 System.out.println("Hey,it is a test");
+		System.out.println("Hey,changes from git");
 	}
 
 }
